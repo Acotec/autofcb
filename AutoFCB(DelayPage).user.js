58e0545d812ff35fb4e73ddbfcec698e17250b38
HARFHO <Harfho77@gmail.com>
2021-06-07 04:37:42 +0000

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoFCB(DelayPage)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.7.3
+// @version      0.7.4
 // @description  Delay some shortlink page loading time
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(DelayPage).user.js
@@ -13,17 +13,20 @@
 (function() {
     'use strict';
     // Your code here...
-    var delayOn = ['express-cut','bitlinks','neonlink.net','faucet.100count.net','bitcoinly.in',
+   var delayOn = ['express-cut','bitlinks','neonlink.net','faucet.100count.net','bitcoinly.in',
                    'kiiw.icu','adbull.me','Linko','Clickit','owllink.net','pingit','cashurl',
                    'adshort','aii.sh','fc.lc','riful']
-    var host= window.location.host.toLowerCase().replace(/https:\/\/|www./,'')
+    var host= window.location.host.toLowerCase()
     function sleep (seconds) {
         var start = new Date().getTime();
         while (new Date() < start + seconds*1000){};
         return 0;
     }
-    if(delayOn.includes(host)){
-        sleep(16)
+    for(const link of delayOn){
+        if(host.includes(link.toLowerCase() ) ){
+            sleep(15);
+        }
     }
+
 
 })();
