f153a1f9db11e7a33595c548723bf8f597c4a438
HARFHO <Harfho77@gmail.com>
2021-06-10 12:22:22 -0600

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,29 +1,20 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.3
+// @version      1.0.5
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
-// @include     *auto*/dashboard/shortlinks*
+// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @include     *auto*/dashboard/shortlinks
 // @grant        window.close
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @run-at       document-end
+// @run-at       document-start
 //// @require      http://code.jquery.com/jquery-3.5.1.min.js
 // ==/UserScript==
 (function () {
     'use strict';
-    var Nav = (() => {
-        var navigate = performance.getEntriesByType("navigation")[0].redirectCount
-        var redirect = document.referrer.includes('shortlinks/visited/')
-        var success = document.querySelector('div.alert-success') !== null
-        if (navigate == 1 || redirect == true || success == true) {
-            window.close()
-            window.close()
-        }
-
-    })()
-
-})();
+    var Nav=(()=>{var e=performance.getEntriesByType("navigation")[0].redirectCount,r=document.referrer.includes("shortlinks/visited/");1==e||1==r?(window.close(),window.close()):waitForKeyElements("div.alert-success",e=>{window.close()})})();
+    })();
