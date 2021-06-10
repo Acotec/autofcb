26738689578e74989fada8d87eba31ccece431fc
HARFHO <Harfho77@gmail.com>
2021-06-10 01:58:27 +0000

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.7
+// @version      1.0.3
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
@@ -10,21 +10,20 @@
 // @grant        window.close
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @run-at       document-start
+// @run-at       document-end
 //// @require      http://code.jquery.com/jquery-3.5.1.min.js
 // ==/UserScript==
-
-(function() {
+(function () {
     'use strict';
-    var Nav=(()=>{
-        var navigate = performance.getEntriesByType("navigation")[0].type
+    var Nav = (() => {
+        var navigate = performance.getEntriesByType("navigation")[0].redirectCount
         var redirect = document.referrer.includes('shortlinks/visited/')
-        var success = document.getElementsByClassName('alert-success').length>0;
-        if(navigate=='navigate'||redirect==true||success==true){
+        var success = document.querySelector('div.alert-success') !== null
+        if (navigate == 1 || redirect == true || success == true) {
             window.close()
             window.close()
         }
 
     })()
 
-    })();
+})();
