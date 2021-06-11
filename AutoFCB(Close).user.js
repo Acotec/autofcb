e6d92577df88771e39cd8110b555f3b2fa57368e
HARFHO <Harfho77@gmail.com>
2021-06-11 05:02:14 +0000

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.5
+// @version      1.0.3
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
 // @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
-// @include     *auto*/dashboard/shortlinks
+// @include      *auto*/dashboard/shortlinks
 // @grant        window.close
-// @grant        GM_setValue
-// @grant        GM_getValue
+// @grant        unsafeWindow
 // @run-at       document-start
-//// @require      http://code.jquery.com/jquery-3.5.1.min.js
 // ==/UserScript==
+
 (function () {
     'use strict';
-    var Nav=(()=>{var e=performance.getEntriesByType("navigation")[0].redirectCount,r=document.referrer.includes("shortlinks/visited/");1==e||1==r?(window.close(),window.close()):waitForKeyElements("div.alert-success",e=>{window.close()})})();
-    })();
+    waitForKeyElements("div.alert-success", () => {window.close()},true,10,-1)
+
+
+
+})();
