d01f3288cd3b9f1f6c1905767d505f976e5e3f0c
HARFHO <Harfho77@gmail.com>
2021-06-11 06:09:05 +0000

    Update AutoFCB(Remove_Elem).user.js

--- AutoFCB(Remove_Elem).user.js
+++ AutoFCB(Remove_Elem).user.js
@@ -1,15 +1,13 @@
 // ==UserScript==
 // @name         AutoFCB(Remove_Elem)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.4.2
+// @version      0.4.4
 // @description   Remove Element from Page
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Remove_Elem).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Remove_Elem).user.js
-// @include     *auto*/dashboard*
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        window.close
+// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @include      *auto*/dashboard*
 // @run-at       document-start
 // ==/UserScript==
 
@@ -17,33 +15,21 @@
 (function() {
     'use strict';
     var h = window.location.href
-    // 1. Create the button
-    var button = document.createElement("button");
-    // 2. Append somewhere
 
-    function removeElement(){
-        try{ var elem = document.getElementsByClassName('anticon anticon-logout icon')[0].parentElement.parentElement
+    function removeElement(element){
+        var elem = element.parentElement.parentElement
         elem.parentNode.removeChild(elem)//remove log-out button from every where;
-           }catch(err){null}
 
         if(h.includes('withdraw/')){
             var ele = Array.from(document.querySelectorAll('option'))
             ele.forEach(element =>{if(element.innerHTML.toLowerCase().includes('expresscrypto')){element.remove()}});; //remove withdrawal to ExpressCrypto
-            //document.getElementById('settings').remove()//remove withdrawal settings
         }
     }
 
-    try{
-        var inter = setInterval(()=>{
-            removeElement()
-            clearInterval(inter)
+    waitForKeyElements(".anticon-logout", (element) => {
+        removeElement(element)
+    }, );
 
-        },)
-        }
-    catch(err){
-        null
-        clearInterval(inter)
-    }
 
 
 })();
