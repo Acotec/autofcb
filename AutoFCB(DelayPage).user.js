2f5010d54f9ec51a92101dbc12a4db559a388aef
HARFHO <Harfho77@gmail.com>
2021-06-15 17:58:46 +0000

    Update AutoFCB(DelayPage).user.js

--- AutoFCB(DelayPage).user.js
+++ AutoFCB(DelayPage).user.js
@@ -19,10 +19,11 @@
     'use strict';
     // Your code here...
     var element, error1052, alreadyVisit, success, referrer;
-    var default_host =['express-cut.ovh', 'bitlinks.pw', 'neonlink.net', 'faucet.100count.net', 'bitcoinly.in',
-                       'kiiw.icu', 'adbull.me', 'owllink.net', 'pingit.im', 'cashurl.in',
-                       'adshort.co', 'aii.sh', 'riful.com','fc.lc',
-                      ]
+    var default_host =[]
+    // ['express-cut.ovh', 'bitlinks.pw', 'neonlink.net', 'faucet.100count.net', 'bitcoinly.in',
+    //                'kiiw.icu', 'adbull.me', 'owllink.net', 'pingit.im', 'cashurl.in',
+    //                'adshort.co', 'aii.sh', 'riful.com','fc.lc',
+    //               ]
     var delayOn = GM_SuperValue.get('delayOn', default_host)
     var host = window.location.host.toLowerCase().replace(/https:\/\/|www\.|\[^.*]|\/.*/ig, '')
     var autofcb = /auto(faucet|claim|bitco)/ig.test(host)
@@ -40,7 +41,7 @@
         if (error1052 && !(referrer.includes('auto') || delayOn.includes(referrer) || referrer == '')) {
             delayOn.push(host)
             GM_SuperValue.set('delayOn', delayOn);
-        } else if (referrer == '' && !alreadyVisit && autofcb && !delayOn.includes(referrer)) {
+        } else if (referrer == '' && autofcb && !(alreadyVisit  || delayOn.includes(referrer))){
             window.history.go(-1)
             //window.history.back()
         } else {
@@ -51,7 +52,7 @@
         waitForKeyElements("div.alert-danger", (element) => {
             addDelayorClose(element)
         }, );
-    } else if (back && !delayOn.includes(host) && document.referrer.includes('auto')==true && !autofcb) {
+    } else if (back && document.referrer.includes('auto') && !(delayOn.includes(host) || host.includes('auto'))) {
         delayOn.push(host)
         GM_SuperValue.set('delayOn', delayOn);
     }else if (delayOn.includes(host)) {
