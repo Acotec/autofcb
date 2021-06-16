90b530a8b4ecdf2a2965f6805bd2123b08928cee
HARFHO <Harfho77@gmail.com>
2021-06-16 14:28:23 -0200

    Update AutoFCB(Reload).user.js

--- AutoFCB(Reload).user.js
+++ AutoFCB(Reload).user.js
@@ -1,15 +1,18 @@
 // ==UserScript==
 // @name         AutoFCB(Reload)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.0.1
-// @description  relaod page if still for more than a certain time
+// @version      0.0.2
+// @description  relaod page if cease for more than a certain time
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Reload).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Reload).user.js
 // @include      *auto*/dashboard/shortlinks/visited*
+// @grant        window.onurlchange
 // ==/UserScript==
 
 (function() {
     'use strict';
-      setInterval(() => window.location.reload(), 10000);
+    setInterval(() => window.location.reload(false), 30*1000);
+    document.title = 'Reload '+document.title
+
 })();
