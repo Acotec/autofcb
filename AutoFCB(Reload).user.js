4f4407fd2e241f716cc03487d92d8d08a1065d03
HARFHO <Harfho77@gmail.com>
2021-06-26 03:45:46 -1000

    Update AutoFCB(Reload).user.js

--- AutoFCB(Reload).user.js
+++ AutoFCB(Reload).user.js
@@ -1,27 +1,18 @@
 // ==UserScript==
 // @name         AutoFCB(Reload)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      0.0.2
+// @version      0.0.4
 // @description  relaod page if cease for more than a certain time
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Reload).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Reload).user.js
-// @include      *auto*/dashboard/shortlinks/visited*
-// @run-at       document-start
+// @include      *autobitco*/dashboard/shortlinks/visited*
+// @run-at       document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
-    function countDown(i, callback) {
-        callback = callback || function(){};
-        var int = setInterval(function() {
-            document.title='Reload '+ i
-            i-- || (clearInterval(int), callback());
-        }, 1000);
-    }
-
-    countDown(60, function(){
-        window.location.reload(false)
-    });
+    document.title='R-'+document.title
+    setTimeout(()=>{location.reload(false)},1000*15)
 
 })();
