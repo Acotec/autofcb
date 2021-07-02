d17ae6e313c23035edaac77efaec36419b7c5b2f
HARFHO <Harfho77@gmail.com>
2021-07-01 18:29:43 -1000

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.4
+// @version      1.0.5
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
@@ -9,11 +9,21 @@
 // @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
 // @include      *auto*/dashboard/shortlinks
 // @grant        window.close
-// @grant        unsafeWindow
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
     'use strict';
-    var Nav=(()=>{var e=performance.getEntriesByType("navigation")[0].redirectCount,r=document.referrer.includes("shortlinks/visited/");1==e||1==r?(window.close(),window.close()):waitForKeyElements("div.alert-success",e=>{window.close()},true,10,-1)})();
-    })();
+    var navigate = performance.getEntriesByType("navigation")[0].type == 'navigate'
+    var ref_self = /auto(faucet|claim|bitco)/ig.test(document.referrer)
+    var referrer = document.referrer.includes("shortlinks/visited/");
+    if ((navigate && !ref_self) || referrer ) {
+        window.close()
+    }else {
+        waitForKeyElements("div.alert-success", function(e) {
+            window.close();
+        }, true, 10, -1);
+    }
+
+
+})();
