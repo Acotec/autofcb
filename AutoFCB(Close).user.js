eecefba9e2ed049c703cd5e8522fb14c4a51be29
HARFHO <Harfho77@gmail.com>
2021-07-05 12:37:33 -0300

    Update AutoFCB(Close).user.js

--- AutoFCB(Close).user.js
+++ AutoFCB(Close).user.js
@@ -1,29 +1,20 @@
 // ==UserScript==
 // @name         AutoFCB(Close)
 // @namespace    https://github.com/Acotec/autofcb
-// @version      1.0.5
+// @version      1.0.7
 // @description  Auto closeDone Auto**/
 // @author       Acotec
 // @updateURL    https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
 // @downloadURL  https://github.com/Acotec/autofcb/raw/master/AutoFCB(Close).user.js
-// @require      https://github.com/Acotec/require/raw/master/waitForKeyElements.min.js
+// @require      https://github.com/Acotec/require/raw/master/cookie_sg_min.js
 // @include      *auto*/dashboard/shortlinks
 // @grant        window.close
 // @run-at       document-start
 // ==/UserScript==
 
 (function () {
-    'use strict';
-    var navigate = performance.getEntriesByType("navigation")[0].type == 'navigate'
-    var ref_self = /auto(faucet|claim|bitco)/ig.test(document.referrer)
-    var referrer = document.referrer.includes("shortlinks/visited/");
-    if ((navigate && !ref_self) || referrer ) {
-        window.close()
-    }else {
-        waitForKeyElements("div.alert-success", function(e) {
-            window.close();
-        }, true, 10, -1);
+    var c = getCookie("visited");
+    if (c === "yes") {
+        window.close();window.close();window.close();window.close();window.close();window.close()
     }
-
-
-})();
+    setCookie(
